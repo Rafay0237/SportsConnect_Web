91fbc7c52c3dde64c5657d763c8b2068f1d5fb43
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-11-17 00:34:03 +0500

    minor css update

--- src/components/request_messages/components/DropdownMenu.jsx
+++ src/components/request_messages/components/DropdownMenu.jsx
@@ -1,6 +1,6 @@
 export default function DropdownMenu({ isOpen, children }) {
     return isOpen ? (
-      <div className="min-h-60 absolute right-0 mt-2 w-80 bg-white rounded-md shadow-lg z-50">
+      <div className="min-h-60 absolute -right-12 sm:right-0 mt-2 w-80 bg-white rounded-md shadow-lg z-50">
         {children}
       </div>
     ) : null;
